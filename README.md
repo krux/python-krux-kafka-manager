9e890dff58e96bab9e0dd6a33eeb5d8162f51247
Kareena Hirani <khirani@krux.com>
2016-07-19 16:19:35 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+python-krux-kafka-manager
+===============
---
+++ krux_kafka_manager/kafka_manager.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# © 2016 Krux Digital, Inc.
+#
+"""
+Class to retrieve data from Kafka clusters
+"""
+
+#
+# Standard libraries
+#
+
+#
+# Third party libraries
+#
+
+
+#
+# Internal libraries
+#
+
+
