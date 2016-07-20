8fef5d1f8320ff1ed7040e1a1068f87c960cb9ff
Kareena Hirani <khirani@krux.com>
2016-07-20 09:56:40 -0700

    Basic CLI format created

--- krux_kafka_manager/cli.py
+++ krux_kafka_manager/cli.py
@@ -23,5 +23,39 @@ from __future__ import absolute_import
 import krux.cli
 from krux.logging import get_logger
 from krux.cli import get_group
-from krux_kafka_manager.kafka_manager import NAME, KafkaManager # Import ApplicationName
+from krux_kafka_manager.kafka_manager import NAME, KafkaManager, get_kafka_manager
+
+
+class Application(krux.cli.Application):
+    def __init__(self, name=NAME):
+        # Call to the superclass to bootstrap.
+        super(Application, self).__init__(name=name)
+
+        self.logger = get_logger(name)
+
+        self.kafka_manager = get_kafka_manager(args-self.args, logger=self.logger, stats=self.stats)
+
+    def add_cli_arguments(self, parser):
+        """
+        Add KafkaManager-related command-line arguments to the given parser.
+
+        :argument parser: parser instance to which the arguments will be added
+        """
+        add_kafka_manager_cli_arguments(parser)
+
+        group = get_group(parser, self.name)
+
+    def run(self):
+        get_brokers_skew = self.kafka_manager.get_brokers_skew()
+        self.logger.debug(get_brokers_skew)
+
+
+def main():
+    app = Application()
+    with app.context():
+        app.run()
+
+# Run the application stand alone
+if __name__ == '__main__':
+    main()
 
