f4a59f3eacc4691489136b48bb313648f548b8fe
Kelsey Lam <kelseylam@berkeley.edu>
2016-07-21 13:31:00 -0700

    Finished CLI test

--- test/cli_test.py
+++ test/cli_test.py
@@ -24,59 +24,51 @@ from mock import MagicMock, patch
 from krux.stats import DummyStatsClient
 from krux_kafka_manager.cli import Application, NAME, main
 from krux_kafka_manager.kafka_manager_api import KafkaManagerAPI
-from krux.cli import get_group
 
 
-@patch('sys.argv', ['krux-kafka', 'host'])
 class CLItest(unittest.TestCase):
+
+    _BROKER_SKEW_INT = 0
+
+    @patch('krux_kafka_manager.cli.krux.cli.krux.logging.get_logger')
+    @patch('krux_kafka_manager.cli.get_kafka_manager_api')
+    @patch('sys.argv', ['krux-kafka', 'http://localhost:9000'])
+    def setUp(self, mock_get_manager, mock_get_logger):
+        self.app = Application()
+        self.mock_get_manager = mock_get_manager
+        self.mock_get_logger = mock_get_logger
 
     def test_init(self):
         """
         CLI constructor creates all the required private properties
         """
-        self.app = Application()
-
         # There are not much we can test except all the objects are under the correct name
         self.assertEqual(NAME, self.app.name)
         self.assertEqual(NAME, self.app.parser.description)
         # The dummy stats client has no awareness of the name. Just check the class.
         self.assertIsInstance(self.app.stats, DummyStatsClient)
-        self.assertEqual(NAME, self.app.logger.name)
 
-        self.assertIsInstance(self.app.kafka_manager_api, KafkaManagerAPI)
-
-    @patch('krux_kafka_manager.cli.get_kafka_manager_api')
-    def test_init_all_set(self, mock_getter):
-        """
-        Kafka Manager API is created properly.
-        """
-        app = Application()
-
-        mock_getter.assert_called_once_with(
-            args=app.args,
-            logger=app.logger,
-            stats=app.stats
+        self.mock_get_manager.assert_called_once_with(
+            args=self.app.args,
+            logger=self.app.logger,
+            stats=self.app.stats
         )
 
     def test_add_cli_arguments(self):
         """
         All arguments from Kafka Manager API are present in the args
         """
-        app = Application()
-
-        self.assertIn('hostname', app.args)
+        self.assertIn('hostname', self.app.args)
 
     def test_run(self):
         """
-        Kafka Manager API's get_brokers_skew method is correctly called in app.run()
+        Kafka Manager API's get_brokers_skew method is correctly called in self.app.run()
         """
-        manager = MagicMock()
+        self.mock_get_manager().get_brokers_skew.return_value = CLItest._BROKER_SKEW_INT
+        self.app.run()
 
-        with patch('krux_kafka_manager.cli.get_kafka_manager_api', return_value=manager):
-            app = Application()
-            app.run()
-
-        manager.get_brokers_skew.assert_called_once_with('krux-manager-test', 'test')
+        self.mock_get_manager().get_brokers_skew.assert_called_once_with('krux-manager-test', 'test')
+        self.mock_get_logger().info.assert_called_once_with(CLItest._BROKER_SKEW_INT)
 
     def test_main(self):
         """
